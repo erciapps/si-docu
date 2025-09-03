7925424a51be6590429458ad6423e262c19a68dc
profe.dam2 <profe.dam2@gmail.com>
2025-09-03 22:41:37 +0200

    Incluir react player

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Sistemas Informáticos',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
